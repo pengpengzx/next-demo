35e5fda2e392062ae1e525ac7bdd2fef53a7062f
张业鹏 <hellolittlewhite@gmail.com>
2021-12-29 10:15:39 +0800

    test

---
+++ playwright.config.ts
@@ -0,0 +1,11 @@
+// playwright.config.ts
+import { PlaywrightTestConfig } from '@playwright/test';
+const config: PlaywrightTestConfig = {
+  webServer: {
+    command: 'npm run start',
+    port: 3000,
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
+};
+export default config;
