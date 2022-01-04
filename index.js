5ccf7f91f632178c76ac7d52e70c6119e1ac204a
张业鹏 <hellolittlewhite@gmail.com>
2022-01-04 11:01:55 +0800

    sd

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ try {
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-
   const { owner, repo } = github.context.repo;
   const { issue_number } = github.context.issue;
   console.log(owner, repo);
